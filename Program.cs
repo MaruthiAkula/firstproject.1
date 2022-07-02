bfc1cdfa2d94e8cb0f914c5a712fb75c3e609c62
MaruthiAkula <99320781+MaruthiAkula@users.noreply.github.com>
2022-07-02 16:21:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstproject._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? Tickets_on_Sale = null;
+            int available_tickets;
+            if (Tickets_on_Sale == null)
+            {
+                available_tickets = 0;
+            }
+            else
+            {
+                available_tickets = (int)Tickets_on_Sale;
+            }
+            Console.WriteLine("available Ticket is {0}", available_tickets);
+        }
+    }
+}
